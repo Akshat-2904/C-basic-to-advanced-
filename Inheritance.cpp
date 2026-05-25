27eb5bb8003ebf4a8d41d7e0fa88e9b36e40bcf9
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 21:01:30 +0530

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,45 +1,35 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//
-//#include <cstdint>
-//
-//
-//class Entity {
-//
-//public:
-//    float x, y;
-//
-//    void move(float xa, float ya)
-//    {
-//        x += xa;
-//        y += ya;
-//    }
-//};
-//
-//class Player : public Entity
-//{
-//public:
-//    const  char* Name;
-//
-//    void PrintName()
-//    {
-//        std::printf(Name);
-//    }
-//
-//
-//};
-//
-//
-//
-//
-//
-//
-//int main() {
-//
-//
-//
-//
-//
-//
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+
+#include <cstdint>
+
+
+class Entity {
+
+public:
+   float x, y;
+
+   void move(float xa, float ya)
+   {
+       x += xa;
+       y += ya;
+   }
+};
+
+class Player : public Entity
+{
+public:
+   const  char* Name;
+
+   void PrintName()
+   {
+       std::printf(Name);
+   }
+
+
+};
+
+int main() {
+
+}
