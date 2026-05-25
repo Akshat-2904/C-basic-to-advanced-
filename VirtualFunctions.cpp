be0e8a58af3b6fa63bb17884d16e8238c80f9b69
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 21:05:38 +0530

    Update VirtualFunctions.cpp

--- VirtualFunctions.cpp
+++ VirtualFunctions.cpp
@@ -1,49 +1,49 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//
-//#include <cstdint>
-//
-//
-//class Entity {
-//
-//public:
-//    virtual std::string GetName() { return "Entity"; }
-//
-//};
-//
-//class Player : public Entity
-//{
-//private:
-//    std::string M_Name;
-//
-//public:
-//    Player(const std::string& name)
-//        :M_Name(name) {
-//    }
-//
-//
-//    std::string GetName() override { return M_Name; }
-//
-//
-//};
-//
-//void PrintName(Entity* En)
-//{
-//    std::cout << En->GetName() << std::endl;
-//}
-//
-//
-//
-//
-//int main() {
-//
-//
-//    Entity* e = new Entity();
-//    Player* p = new Player("Ace");
-//    PrintName(e);
-//    PrintName(p);
-//
-//
-//
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+
+#include <cstdint>
+
+
+class Entity {
+
+public:
+   virtual std::string GetName() { return "Entity"; }
+
+};
+
+class Player : public Entity
+{
+private:
+   std::string M_Name;
+
+public:
+   Player(const std::string& name)
+       :M_Name(name) {
+   }
+
+
+   std::string GetName() override { return M_Name; }
+
+
+};
+
+void PrintName(Entity* En)
+{
+   std::cout << En->GetName() << std::endl;
+}
+
+
+
+
+int main() {
+
+
+   Entity* e = new Entity();
+   Player* p = new Player("Ace");
+   PrintName(e);
+   PrintName(p);
+
+
+
+}
