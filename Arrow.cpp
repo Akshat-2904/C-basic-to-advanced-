9ba542963825b24bbc9284f9535950125236a3d1
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 20:58:37 +0530

    Update Arrow.cpp

--- Arrow.cpp
+++ Arrow.cpp
@@ -1,67 +1,67 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//#include "string"
-//#include <cstdint>
-//#include "stdlib.h"
-//
-//class Entity
-//{
-//public:
-//	void  Print() const { std::cout << "hello" << std::endl; }
-//
-//};
-//struct Vector3
-//{
-//	float x, y, z;
-//};
-//
-//class Scopedptr
-//{
-//private:
-//	Entity* MObj;
-//public:
-//	Scopedptr(Entity* e) :MObj(e)
-//	{
-//
-//	}
-//
-//	~Scopedptr()
-//	{
-//		delete MObj;
-//	}
-//
-//	Entity* operator->()
-//	{
-//		return MObj;
-//	}
-//
-//	const  Entity* operator->()const
-//	{
-//		return MObj;
-//	}
-//
-//};
-//
-//int main()
-//{
-//	const Scopedptr entity(new Entity());
-//
-//
-//
-//	entity->Print();
-//
-//
-//	int offset = (int)&((Vector3*)nullptr)->x;//finding memory offset
-//
-//	int offset2 = (int)&((Vector3*)nullptr)->y;
-//
-//
-//	int offset3 = (int)&((Vector3*)nullptr)->z;
-//
-//	std::cout << offset << " " << offset2 << " " << offset3 << std::endl;
-//
-//
-//
-//
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+#include "string"
+#include <cstdint>
+#include "stdlib.h"
+
+class Entity
+{
+public:
+	void  Print() const { std::cout << "hello" << std::endl; }
+
+};
+struct Vector3
+{
+	float x, y, z;
+};
+
+class Scopedptr
+{
+private:
+	Entity* MObj;
+public:
+	Scopedptr(Entity* e) :MObj(e)
+	{
+
+	}
+
+	~Scopedptr()
+	{
+		delete MObj;
+	}
+
+	Entity* operator->()
+	{
+		return MObj;
+	}
+
+	const  Entity* operator->()const
+	{
+		return MObj;
+	}
+
+};
+
+int main()
+{
+	const Scopedptr entity(new Entity());
+
+
+
+	entity->Print();
+
+
+	int offset = (int)&((Vector3*)nullptr)->x;finding memory offset
+
+	int offset2 = (int)&((Vector3*)nullptr)->y;
+
+
+	int offset3 = (int)&((Vector3*)nullptr)->z;
+
+	std::cout << offset << " " << offset2 << " " << offset3 << std::endl;
+
+
+
+
+}
