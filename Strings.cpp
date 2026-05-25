829797eba39c733c6619c62c6760d6ea485a2c38
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 21:05:14 +0530

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -1,26 +1,26 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//#include "string"
-//#include <cstdint>
-//
-//
-//
-//void PrintString(const std::string& msg)//using ref will save memory
-//{
-//    std::cout << msg << std::endl;
-//
-//}
-//int main() {
-//
-//    std::string name = "Ace";//const as string is immutable
-//    name += "Ashborn";//adding ,we cannot add in single,in single line we have to specify both values
-//    //
-//    // std::string name = "Ace" +std::string("Ashborn");
-//
-//    std::cout << name << std::endl;//cannot use printf
-//
-//    bool contains = name.find("ce") != std::string::npos;//will check if ce is present and if it is at valid address(npos).
-//
-//    PrintString(name);
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+#include "string"
+#include <cstdint>
+
+
+
+void PrintString(const std::string& msg)//using ref will save memory
+{
+   std::cout << msg << std::endl;
+
+}
+int main() {
+
+   std::string name = "Ace";//const as string is immutable
+   name += "Ashborn";//adding ,we cannot add in single,in single line we have to specify both values
+   //
+   // std::string name = "Ace" +std::string("Ashborn");
+
+   std::cout << name << std::endl;//cannot use printf
+
+   bool contains = name.find("ce") != std::string::npos;//will check if ce is present and if it is at valid address(npos).
+
+   PrintString(name);
+}
