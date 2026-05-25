237cb9690678f46226baff6a1e685e11c2f75965
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 21:06:03 +0530

    Update const.cpp

--- const.cpp
+++ const.cpp
@@ -1,42 +1,42 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//#include "string"
-//#include <cstdint>
-//#include "stdlib.h"
-//
-//class Entity
-//{
-//private:
-//    int Mx, My;
-//    int* mx, * my;//decalring two pointers
-//    mutable int var;
-//public:
-//    int GetX() const/// using const we ensure that this function does not modify the member variables in any way
-//    {
-//
-//        var = 5;
-//        return Mx;
-//
-//    }
-//
-//};
-//
-//
-//
-//void PrintEntity(const Entity& e)//using ref will save memory
-//{
-//    std::cout << e.GetX() << std::endl;
-//
-//}
-//int main() {
-//
-//    int ab = 0;
-//    int ac = 89;
-//    int* a = new int;
-//    a = &ac;
-//
-//    *a = 67;
-//    std::printf("%d", ac);
-//
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+#include "string"
+#include <cstdint>
+#include "stdlib.h"
+
+class Entity
+{
+private:
+   int Mx, My;
+   int* mx, * my;//decalring two pointers
+   mutable int var;
+public:
+   int GetX() const/// using const we ensure that this function does not modify the member variables in any way
+   {
+
+       var = 5;
+       return Mx;
+
+   }
+
+};
+
+
+
+void PrintEntity(const Entity& e)//using ref will save memory
+{
+   std::cout << e.GetX() << std::endl;
+
+}
+int main() {
+
+   int ab = 0;
+   int ac = 89;
+   int* a = new int;
+   a = &ac;
+
+   *a = 67;
+   std::printf("%d", ac);
+
+}
