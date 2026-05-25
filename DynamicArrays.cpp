7449e4ea02453acb9fd7e98884e1d490174a1a2b
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 20:58:17 +0530

    Update DynamicArrays.cpp

--- DynamicArrays.cpp
+++ DynamicArrays.cpp
@@ -1,35 +1,35 @@
-//#include"iostream"
-//#include "vector"
-//struct Vertex
-//{
-//	float x, y, z;
-//};
-//
-//std::ostream& operator <<(std::ostream& stream, const Vertex& vertex)
-//{
-//	stream << vertex.x << "," << vertex.y << "," << vertex.z;
-//	return stream;
-//}
-//
-//int main()
-//{
-//	std::vector<Vertex>Vertices;
-//	Vertices.push_back({ 1, 3, 4 });
-//	Vertices.push_back({ 1, 9, 4 });
-//
-//
-//	for (const Vertex& v : Vertices)//using const will avoid copying in v
-//		std::cout << v << std::endl;
-//
-//	Vertices.erase(Vertices.begin() + 1);
-//
-//	std::cout << Vertices.size() << std::endl;
-//	for (int i = 0; i < Vertices.size(); i++)
-//	{
-//
-//		std::cout << Vertices[i] << std::endl;
-//	}
-//
-//
-//}
-//// always pas array in function using &(ref)
+#include"iostream"
+#include "vector"
+struct Vertex
+{
+	float x, y, z;
+};
+
+std::ostream& operator <<(std::ostream& stream, const Vertex& vertex)
+{
+	stream << vertex.x << "," << vertex.y << "," << vertex.z;
+	return stream;
+}
+
+int main()
+{
+	std::vector<Vertex>Vertices;
+	Vertices.push_back({ 1, 3, 4 });
+	Vertices.push_back({ 1, 9, 4 });
+
+
+	for (const Vertex& v : Vertices)using const will avoid copying in v
+		std::cout << v << std::endl;
+
+	Vertices.erase(Vertices.begin() + 1);
+
+	std::cout << Vertices.size() << std::endl;
+	for (int i = 0; i < Vertices.size(); i++)
+	{
+
+		std::cout << Vertices[i] << std::endl;
+	}
+
+
+}
+ always pas array in function using &(ref)
