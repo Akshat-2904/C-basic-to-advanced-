38bf9e8e97ac86a461079005dbf35b064fb01d4b
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 21:05:25 +0530

    Update TernaryOp.cpp

--- TernaryOp.cpp
+++ TernaryOp.cpp
@@ -1,24 +1,24 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//#include "string"
-//#include <cstdint>
-//#include "stdlib.h"
-//
-//
-//
-//static int s_Level = 1;// ststic ensures that this varible is only for this file
-//// always use static if these variables are global and are not going be used else where 
-////in another file
-//
-//static int s_Speed = 2;
-//
-//
-//
-//
-//int main() {
-//
-//    s_Speed = s_Level > 5 ? 10 : 5;
-//    std::string Rank = s_Level > 100 ? "Master" : "Noob";
-//
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+#include "string"
+#include <cstdint>
+#include "stdlib.h"
+
+
+
+static int s_Level = 1;// ststic ensures that this varible is only for this file
+// always use static if these variables are global and are not going be used else where 
+//in another file
+
+static int s_Speed = 2;
+
+
+
+
+int main() {
+
+   s_Speed = s_Level > 5 ? 10 : 5;
+   std::string Rank = s_Level > 100 ? "Master" : "Noob";
+
+}
