6f67086c7f9be9914bc6a17a65cd8e82edb9a527
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 21:05:49 +0530

    Update Workin_of_SmartPtr.cpp

--- Workin_of_SmartPtr.cpp
+++ Workin_of_SmartPtr.cpp
@@ -1,49 +1,49 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//#include "string"
-//#include <cstdint>
-//#include "stdlib.h"
-//
-//using String = std::string;
-//class Entity
-//{
-//
-//public:
-//	Entity()
-//	{
-//		std::cout << "Object Created" << std::endl;
-//	}
-//
-//	~Entity()
-//	{
-//		std::cout << "Object Destroyed" << std::endl;
-//	}
-//};
-//
-//class SmartPtr
-//{
-//private:
-//	Entity* Mptr;
-//public:
-//	SmartPtr(Entity* ptr) :Mptr(ptr)
-//	{
-//
-//	}
-//
-//	~SmartPtr()
-//	{
-//
-//		delete Mptr;
-//	}
-//
-//
-//};
-//
-//int main() {
-//
-//	{
-//		SmartPtr e(new Entity());// SmartPtr e=new Entity();
-//		//though create on heap the ptr was on stack, as the ptr go deleted the deconctructor of Smartprt got called which deleted e. 
-//	}
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+#include "string"
+#include <cstdint>
+#include "stdlib.h"
+
+using String = std::string;
+class Entity
+{
+
+public:
+	Entity()
+	{
+		std::cout << "Object Created" << std::endl;
+	}
+
+	~Entity()
+	{
+		std::cout << "Object Destroyed" << std::endl;
+	}
+};
+
+class SmartPtr
+{
+private:
+	Entity* Mptr;
+public:
+	SmartPtr(Entity* ptr) :Mptr(ptr)
+	{
+
+	}
+
+	~SmartPtr()
+	{
+
+		delete Mptr;
+	}
+
+
+};
+
+int main() {
+
+	{
+		SmartPtr e(new Entity());// SmartPtr e=new Entity();
+		//though create on heap the ptr was on stack, as the ptr go deleted the deconctructor of Smartprt got called which deleted e. 
+	}
+}
