ee8854bc7d2f3204c5426469749d0420251c7434
Akshat-2904 <akshat202489@gmail.com>
2026-05-25 21:05:04 +0530

    Update StringLiterals.cpp

--- StringLiterals.cpp
+++ StringLiterals.cpp
@@ -1,31 +1,31 @@
-//#include <iostream>
-//#include<print>
-//#include "Log.h"
-//#include "string"
-//#include <cstdint>
-//#include "stdlib.h"
-//
-//
-//
-//static int s_Level = 1;// ststic ensures that this varible is only for this file
-//// always use static if these variables are global and are not going be used else where 
-////in another file
-//
-//static int s_Speed = 2;
-//
-//
-//
-//
-//int main() {
-//
-//    s_Speed = s_Level > 5 ? 10 : 5;
-//
-//    using namespace std::string_literals;
-//    std::string s = R"(
-//       Multiple lines 
-//      made easy.
-//    )";
-//
-//
-//    s = "New"s + "Ace";// direct adding possible
-//}
+#include <iostream>
+#include<print>
+#include "Log.h"
+#include "string"
+#include <cstdint>
+#include "stdlib.h"
+
+
+
+static int s_Level = 1;// ststic ensures that this varible is only for this file
+// always use static if these variables are global and are not going be used else where 
+//in another file
+
+static int s_Speed = 2;
+
+
+
+
+int main() {
+
+   s_Speed = s_Level > 5 ? 10 : 5;
+
+   using namespace std::string_literals;
+   std::string s = R"(
+      Multiple lines 
+     made easy.
+   )";
+
+
+   s = "New"s + "Ace";// direct adding possible
+}
